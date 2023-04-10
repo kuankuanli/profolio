86bac32ea0dd7137891cfd095af8e1ba97a34657
kuankuanli <kuan850223@gmail.com>
2023-04-11 01:44:11 +0800

    first version finish!

---
+++ wwwroot/js/Base.js
@@ -0,0 +1,12 @@
+var LineBtn = document.getElementById("Line-btn");
+var WebBtn = document.getElementById("Web-btn");
+var APPBtn = document.getElementById("APP-btn");
+var ScreenBtn = document.getElementById("Screen-btn");
+var TablewareBtn = document.getElementById("Tableware-btn");
+var ProductBtn = document.getElementById("Product-btn");
+var InviteBtn = document.getElementById("Invite-btn");
+var DMBtn = document.getElementById("DM-btn");
+
+function goHome() {
+    window.location.assign("../Home.html");
+}
