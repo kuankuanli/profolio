5f2b5e603714a3adcabecbbc4eb2d131975dc9ca
kuankuanli <kuan850223@gmail.com>
2023-04-13 00:45:09 +0800

    test: 手機轉橫向跳出警語

--- wwwroot/js/Base.js
+++ wwwroot/js/Base.js
@@ -10,3 +10,15 @@ var DMBtn = document.getElementById("DM-btn");
 function goHome() {
     window.location.assign("../Home.html");
 }
+
+//手機轉橫式跳出警語
+$(window).on("deviceorientation resize", function( event ) {
+    if (window.matchMedia("(orientation: portrait)").matches) {
+        $('.overlay-landscape').removeClass('active');
+        $('.overlay-landscape-modal-mobile').removeClass('active');
+    }
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        $('.overlay-landscape').addClass('active');
+        $('.overlay-landscape-modal-mobile').addClass('active');
+    }
+});
